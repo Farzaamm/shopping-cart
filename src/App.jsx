2a02ff9e8bd8a1bd3c81b807fc46093275190b02
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2025-03-19 15:32:11 +0800

    Header, Footer, Home , and Shopping components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Shopping from './components/Shopping'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <div>This is the App</div>
+      <Header />
+      <Home /> || <Shopping />
+      <Footer />
     </>
   )
 }
